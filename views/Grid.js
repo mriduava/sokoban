5ee8ba3c4720e783b4c37eaf1e7861dda4f27a61
Heidi <heid_96@outlook.com>
2019-12-04 22:28:47 +0100

    The avatar doesn't go out out of walls.

--- views/Grid.js
+++ views/Grid.js
@@ -41,6 +41,9 @@ export default {
                 ['W', 'W', 'W', ' ', 'W', 'W', ' ', 'W', 'W', ' ', ' ', 'W'],
                 ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
             ],
+            posOfW:[],
+            posOfWX:[],
+            posOfWY:[],
             showAvatar: false
              
          }
@@ -50,21 +53,25 @@ export default {
         //      return this.$store.state.grids;
         //  }
      },
-     methods() {
-         
-         
+     methods() { 
      },
      mounted() {
-        //  this.grids = this.$store.state.grids;
         let obj = document.querySelector('.obj')
-         for(let x=0; x<this.grids.length; x++){
+         for(let x=0; x<this.grids[0].length; x++){
              for(let y=0; y<this.grids.length; y++){
                  if (this.grids[y][x] == 'O') {
                     this.grids[y][x] = 'Hi'
-                    // console.log(this.grids[x][y]);                                                     
+                    // console.log(this.grids[x][y]);                                                 
                  }
-             } 
-            //  console.log(this.grids[2][1]);              
+                 if(this.grids[y][x] == 'W'){
+                     let pair = {xValue:x*32, yValue:y*32}
+                     this.posOfW.push(pair)
+                     this.posOfWX.push(x*32)
+                     this.posOfWY.push(y*32)
+                    //  console.log(JSON.stringify(pair))
+                 }
+                 
+             }              
          }
 
 
@@ -73,18 +80,21 @@ export default {
         let gridWidth = 352;
         let gridHeight = 224;
 
-        let row = 0;
-        let col = 0;
+        let row = 64;
+        let col = 32;
 
         let tarRow = 0;
         let tarCol = 0;
 
-        let objPos = {x: 0, y: 0}
-
+        let objPos = {x: 2, y: 1}
+        obj.style.left = 2*unit + 'px'
+        obj.style.top = 1*unit + 'px'
         let tarPos = {x: 0, y: 0}
         let goalPos = {x: 3, y: 5}
-
-         
+        // let valueOfW = 0;
+        // for(valueOfW of this.posOfW){
+        //     console.log(valueOfW.xValue)
+        // }
         // let obj = document.querySelector('.obj')
 
         let randNumX = Math.floor(Math.random()*6)
@@ -93,85 +103,129 @@ export default {
         let target = document.querySelector('.target')
         let goal = document.querySelector('.goal')
 
-        document.addEventListener('keydown', (e) => { //left-key
-            if (e.keyCode == 37) {
+        document.addEventListener('keydown', (e) => { 
+            if (e.keyCode == 37) {//left-key
                 if (row > 1) {   
-                    row -= unit;  
-
-                    obj.style.left = row + 'px'
-
+                    row -= unit;
                     objPos.x = row/unit
                     objPos.y = col/unit
-
-                    console.log(objPos); 
-
-                    tarPos.x = tarRow/unit
-                    tarPos.y = tarCol/unit
-
-                    if(JSON.stringify(objPos) == JSON.stringify(tarPos) && 0 < tarRow){                        
-                        console.log('Hit');
-                        tarRow -= unit
-                        target.style.left = tarRow + 'px'
-                    }                                       
-                }                            
+                    let valueOfW = 0;
+                    let trueW = true;
+                    for(valueOfW of this.posOfW){
+                        if(valueOfW.xValue == row & valueOfW.yValue == col){
+                            trueW = false;
+                            row += unit;
+                            objPos.x = row*unit
+                            objPos.y = col*unit
+                            return;
+                        }
+                    }
+                    if(trueW){
+                        obj.style.left = row + 'px'
+                        tarPos.x = tarRow/unit
+                        tarPos.y = tarCol/unit
+                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && 0 < tarRow){                        
+                            console.log('Hit');
+                            tarRow -= unit
+                            target.style.left = tarRow + 'px'
+                            }
+                        }
+                    }
             }else if (e.keyCode == 38) { //up-key
                 if (col > 1) {
                     col -= unit;
-                    obj.style.top = col + 'px'
                     objPos.x = row/unit
                     objPos.y = col/unit
-
-                    console.log(objPos); 
+                    let valueOfW = 0;
+                    let trueW = true;
+                    for(valueOfW of this.posOfW){
+                        if(valueOfW.xValue == row & valueOfW.yValue == col){
+                            trueW = false;
+                            col += unit;
+                            objPos.x = row*unit
+                            objPos.y = col*unit
+                            return;
+                        }
+                    }
+                    if(trueW){
+                    obj.style.top = col + 'px'
                     tarPos.x = tarRow/unit
                     tarPos.y = tarCol/unit
-
-                    if(JSON.stringify(objPos) == JSON.stringify(tarPos) && 0 < tarCol){                        
-                        console.log('Hit');
-                        tarCol -= unit
-                        target.style.top = tarCol + 'px'
-                    } 
+                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && 0 < tarCol){                        
+                            console.log('Hit');
+                            tarCol -= unit
+                            target.style.top = tarCol + 'px'
+                        }
+                    }
                 }                
             }else if (e.keyCode == 39) {//right-key
                 if (row < gridWidth) {
                     row += unit;        
-                    obj.style.left = row + 'px'
                     objPos.x = row/unit
                     objPos.y = col/unit
- 
-                    console.log(objPos); 
+
+                    let valueOfW = 0;
+                    let trueW = true;
+                    for(valueOfW of this.posOfW){
+                        if(valueOfW.xValue == row & valueOfW.yValue == col){
+                            trueW = false;
+                            row -= unit;
+                            objPos.x = row*unit
+                            objPos.y = col*unit
+                            return;
+                        }
+                    }
+                    
+                    if(trueW){
+                    obj.style.left = row + 'px' 
                     tarPos.x = tarRow/unit
                     tarPos.y = tarCol/unit
-                    if(JSON.stringify(tarPos) === JSON.stringify(goalPos)){
-                        console.log('Good job!'); 
-                        score ++ 
-                        this.score = score; 
+                        if(JSON.stringify(tarPos) === JSON.stringify(goalPos)){
+                            console.log('Good job!'); 
+                            score ++ 
+                            this.score = score; 
 
 
-                        this.$store.state.score = this.score                    
-                        console.log(this.score);
-                    
-                        // this.updateScore(score)
-                                                 
+                            this.$store.state.score = this.score                    
+                            console.log(this.score);
+                        
+                            // this.updateScore(score)
+                                                    
+                        }
+                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && tarRow < gridWidth){                        
+                            console.log('Hit');
+                            tarRow += unit
+                            target.style.left = tarRow + 'px'                        
+                        } 
                     }
-                    if(JSON.stringify(objPos) == JSON.stringify(tarPos) && tarRow < gridWidth){                        
-                        console.log('Hit');
-                        tarRow += unit
-                        target.style.left = tarRow + 'px'                        
-                    } 
                 }
             }else if (e.keyCode == 40) {//down-key
                 if (col < gridHeight) {
                     col += unit;
-                    obj.style.top = col + 'px'
+                    
                     objPos.x = row/unit
                     objPos.y = col/unit
-  
+
+                    let valueOfW = 0;
+                    let trueW = true;
+                    for(valueOfW of this.posOfW){
+                        if(valueOfW.xValue == row & valueOfW.yValue == col){
+                            trueW = false;
+                            col -= unit;
+                            objPos.x = row*unit
+                            objPos.y = col*unit
+                            return;
+                        }
+                    }
+                    if(trueW){
+                    obj.style.top = col + 'px'
                     tarPos.x = tarRow/unit
                     tarPos.y = tarCol/unit
-                    if(JSON.stringify(objPos) == JSON.stringify(tarPos) && tarCol < gridHeight){                        
-                        console.log('Hit');
-                        tarCol += unit
-                        target.style.top = tarCol + 'px'
+                        if(JSON.stringify(objPos) == JSON.stringify(tarPos)){                        
+                            console.log('Hit');
+                            tarCol += unit
+                            target.style.top = tarCol + 'px'
+                        }
                     }
                 }        
             } 
