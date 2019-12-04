2b2f8f6ee76f0c92a655a84df517ecca8efd1fd9
Heidi <heid_96@outlook.com>
2019-12-04 11:19:21 +0100

    Levels.js to develop.

---
+++ views/Levels.js
@@ -0,0 +1,18 @@
+
+import Grid from './Grid'
+import Board from './Board.js'
+
+
+export default{
+    components: {
+        Board,
+        Grid
+    }
+    template:
+        `
+        
+        <div id="grid"></div>
+        
+        `
+}
+}
