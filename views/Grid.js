3d72189aa19852480aaa27e7799f58df60cc3c08
Heidi <heid_96@outlook.com>
2019-12-05 11:59:17 +0100

    incoming changes

--- views/Grid.js
+++ views/Grid.js
@@ -36,41 +36,10 @@ export default {
                 ['W', 'W', 'W', ' ', 'W', 'W', ' ', ' ', ' ', 'G', 'G', 'W'],
                 ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
             ],
-<<<<<<< HEAD
-            posOfW:[],
-            showAvatar: false
-             
-         }
-     },
-     computed: {
-        //  grids(){
-        //      return this.$store.state.grids;
-        //  }
-     },
-     methods() { 
-     },
-     mounted() {
-        let obj = document.querySelector('.obj')
-         for(let x=0; x<this.grids[0].length; x++){
-             for(let y=0; y<this.grids.length; y++){
-                 if (this.grids[y][x] == 'O') {
-                    this.grids[y][x] = 'Hi'
-                    // console.log(this.grids[x][y]);                                                 
-                 }
-                 if(this.grids[y][x] == 'W'){
-                     let pair = {xValue:x*32, yValue:y*32}
-                     this.posOfW.push(pair)
-                    //  console.log(JSON.stringify(pair))
-                 }
-                 
-             }              
-         }
-=======
             wallPositions: []   
          }
      },
      mounted() {
->>>>>>> master
 
         let avatar = document.querySelector('.avatar')
         let target = document.querySelector('.target')
@@ -81,70 +50,6 @@ export default {
         let gridWidth = 352;
         let gridHeight = 224;
 
-<<<<<<< HEAD
-        let row = 2*unit;
-        let col = 1*unit;
-
-        let tarRow = 5*unit;
-        let tarCol = 3*unit;
-
-        let objPos = {x: 2, y: 1}
-        obj.style.left = 2*unit + 'px'
-        obj.style.top = 1*unit + 'px'
-        let tarPos = {x: 5, y: 3}
-        let goalPos = {x: 3, y: 5}
-        
-
-        // let randNumX = Math.floor(Math.random()*6)
-        // let randNumY = Math.floor(Math.random()*6)
-
-        let target = document.querySelector('.target')
-        target.style.left = 5*unit + 'px';
-        target.style.top = 3*unit + 'px';
-
-        let goal = document.querySelector('.goal')
-
-        document.addEventListener('keydown', (e) => { 
-            if (e.keyCode == 37) {//left-key
-                if (row > 1) {   
-                    row -= unit;
-                    let t = tarRow
-                    t -= unit;
-
-                    objPos.x = row/unit
-                    objPos.y = col/unit
-                    let valueOfW = 0;
-                    let trueW = true;
-                    let trueT = true;
-                    for(valueOfW of this.posOfW){
-                        if(valueOfW.xValue == t & valueOfW.yValue == tarCol){
-                            trueT = false
-                        }
-                        if(valueOfW.xValue == row & valueOfW.yValue == col){
-                            trueW = false;
-                            row += unit;
-                            objPos.x = row*unit
-                            objPos.y = col*unit
-                            return;
-                        }
-                    }
-                    if(trueW){
-                        obj.style.left = row + 'px'
-                        tarPos.x = tarRow/unit
-                        tarPos.y = tarCol/unit
-                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && trueT){                        
-                            console.log('Hit');
-                            tarRow -= unit
-                            target.style.left = tarRow + 'px'
-                            }
-                        }
-                    }
-            }else if (e.keyCode == 38) { //up-key
-                if (col > 1) {
-                    col -= unit;
-                    let t = tarCol
-                    t -= unit;
-=======
         let row = 32;
         let col = 32;
 
@@ -232,7 +137,6 @@ export default {
                     col -= unit;
                     avatar.style.top = col + 'px'
 
->>>>>>> master
                     objPos.x = row/unit
                     objPos.y = col/unit
                     let valueOfW = 0;
@@ -254,20 +158,6 @@ export default {
                     obj.style.top = col + 'px'
                     tarPos.x = tarRow/unit
                     tarPos.y = tarCol/unit
-<<<<<<< HEAD
-                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && trueT){                        
-                            console.log('Hit');
-                            tarCol -= unit
-                            target.style.top = tarCol + 'px'
-                        }
-                    }
-                }                
-            }else if (e.keyCode == 39) {//right-key
-                if (row < gridWidth) {
-                    row += unit;
-                    let t = tarRow
-                    t += unit;       
-=======
 
                     if(JSON.stringify(objPos) == JSON.stringify(tarPos)){                        
                         console.log('Hit');
@@ -281,7 +171,6 @@ export default {
                     row += unit;        
                     avatar.style.left = row + 'px'
 
->>>>>>> master
                     objPos.x = row/unit
                     objPos.y = col/unit
 
@@ -305,25 +194,6 @@ export default {
                     obj.style.left = row + 'px' 
                     tarPos.x = tarRow/unit
                     tarPos.y = tarCol/unit
-<<<<<<< HEAD
-                        if(JSON.stringify(tarPos) === JSON.stringify(goalPos)){
-                            console.log('Good job!'); 
-                            score ++ 
-                            this.score = score; 
-
-
-                            this.$store.state.score = this.score                    
-                            console.log(this.score);
-                        
-                            // this.updateScore(score)
-                                                    
-                        }
-                        if(JSON.stringify(objPos) == JSON.stringify(tarPos) && trueT){                        
-                            console.log('Hit');
-                            tarRow += unit
-                            target.style.left = tarRow + 'px'                        
-                        } 
-=======
 
                     //If the Target & Goals positions are same, it will give One Point
                     if(JSON.stringify(tarPos) === JSON.stringify(goalPos)){
@@ -331,23 +201,14 @@ export default {
                         this.score = score; 
                         //The score in store.js file will be filled with this data
                         this.$store.state.score = this.score                                                 
->>>>>>> master
                     }
                 }
-<<<<<<< HEAD
-            }else if (e.keyCode == 40) {//down-key
-                if (col < gridHeight) {
-                    col += unit;
-                    let t = tarCol
-                    t += unit; 
-=======
             // Down arrow key
             }else if (e.keyCode == 40) {
                 if (col < gridHeight-32 && !existObj(this.wallPositions, objPos)) {
                     col += unit;
                     avatar.style.top = col + 'px'
 
->>>>>>> master
                     objPos.x = row/unit
                     objPos.y = col/unit
 
@@ -380,16 +241,6 @@ export default {
             } 
         })
 
-<<<<<<< HEAD
-        // target.style.left = randNumX*unit + 'px'
-        // target.style.top = randNumY*unit + 'px'
-
-        // tarPos.x = randNumX
-        // tarPos.y = randNumY
-
-        // tarRow = randNumX*unit
-        // tarCol = randNumY*unit
-=======
         /**
         * To give a position of the Target
         * The position is fixed now
@@ -402,7 +253,6 @@ export default {
 
         tarRow = 5*unit
         tarCol = 3*unit
->>>>>>> master
 
         console.log('Target ' + JSON.stringify(tarPos));         
     
