16a6a95b292a72522a13ed2261e04538dd5131f7
Heidi <heid_96@outlook.com>
2019-12-12 02:54:57 +0100

    remove css for .game

--- src/views/Final.js
+++ src/views/Final.js
@@ -1,12 +1,12 @@
-
+import {store} from '../store.js'
 export default {
     template: `
        <div class="final">
            <div class="final-message">
                <div><h1>Thanks for playing SOKOBAN!</h1></div>
                <div>
-                   <h4>Your total score {{fianlScore}}</h4>
-                   <h4>Your played {{levelPlayed}} Levls</h4>
+                   <h4>Your total score {{finalScore}}</h4>
+                   <h4>Your played {{levelPlayed}} levels</h4>
                    <h4>Time spend {{spendTime}} minutes</h4>
                </div>
                <div><h2>Do you want to play again?</h2></div>
@@ -18,8 +18,8 @@ export default {
        </div>`,
        data() {
            return {
-               fianlScore: 0,
-               levelPlayed: 0,
+               finalScore: this.$store.state.score,
+               levelPlayed: this.$store.state.level,
                spendTime: 0
            }
        },
