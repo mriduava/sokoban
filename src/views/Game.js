5d1893361b5116e979ab28d279ed6d7ba6968a9e
Magnus <m_wellbring@yahoo.com>
2019-12-17 02:39:31 +0100

    Avatar and blocks can no longer move out of bounds
    Added out-of-bounds class to style outside the walls and implement the new functionality

--- src/views/Game.js
+++ src/views/Game.js
@@ -8,17 +8,19 @@ export default {
      <div class="grid" @reset="reset()">
         <div v-for="(blockRow, row) in grids">
             <div class="all-blocks" v-for="(block, col) in blockRow" 
-                 :style="{ top: row*unit + 'px', left: col*unit + 'px'}">
-                <div class="walls" v-if="block == 'W'" >
+                 :style="{ top: row*unit + 'px', left: col*unit + 'px'}">      
+                <div class="walls" v-if="block === 'W'">
                 </div>              
                 <div class="goals" v-else-if="block === 'G'" >
                 </div>
                 <div class="boxes" v-else-if="block === 'B'" >
                 </div>
+                <div class="out-of-bounds" v-else-if="block === 'O'" >
+                </div>
             </div>
         </div>
-            <div class="avatar"><img :src="this.avatarImageSrc" width="32" height="32"></div>       
-     </div>`,
+            <div class="avatar" ><img :src="this.avatarImageSrc" width="32" height="32"></div>       
+        </div>`,
     data() {
         return {
             unit: 32,
@@ -26,7 +28,7 @@ export default {
             strengthActive: false,
             bombActive: false,
             drillActive: false,
-            avatarImageSrc: "../public/img/player_down.png",
+            avatarImageSrc: "./public/img/player_down.png",
          }
      },
      mounted() {
@@ -43,6 +45,8 @@ export default {
         let targetPositions = logic.findPositions('B',this.grids)
         let goalPositions = logic.findPositions('G',this.grids)
         let avatarPosition = logic.findPositions('A', this.grids)[0]
+        let outOfBoundsPositions = logic.findPositions('O', this.grids)
+        let collisionPositions = wallPositions.concat(outOfBoundsPositions);
 
         // console.log(goalPositions.length);
         
@@ -71,7 +75,9 @@ export default {
             thisGame.grids = store.grids[store.level].grid 
             avatarPosition = logic.findPositions('A', thisGame.grids)[0]
             wallPositions = logic.findPositions('W',thisGame.grids)
-            targetPositions = logic.findPositions('B',thisGame.grids)                      
+            targetPositions = logic.findPositions('B',thisGame.grids)
+            outOfBoundsPositions = logic.findPositions('O', thisGame.grids)
+            collisionPositions = wallPositions.concat(outOfBoundsPositions)                 
             avatar.style.left = avatarPosition.x * unit + 'px'
             avatar.style.top = avatarPosition.y * unit + 'px'
             target = document.getElementsByClassName('boxes')
@@ -105,7 +111,9 @@ export default {
             avatarPosition = logic.findPositions('A', thisGame.grids)[0]
             wallPositions = logic.findPositions('W',thisGame.grids)
             targetPositions = logic.findPositions('B',thisGame.grids)
-            goalPositions = logic.findPositions('G',thisGame.grids)                        
+            goalPositions = logic.findPositions('G',thisGame.grids)
+            outOfBoundsPositions = logic.findPositions('O', thisGame.grids)
+            collisionPositions = wallPositions.concat(outOfBoundsPositions);
             avatar.style.left = avatarPosition.x * unit + 'px'
             avatar.style.top = avatarPosition.y * unit + 'px'
             listZeroX = [0,0,0,0,0,0]
@@ -121,14 +129,15 @@ export default {
             // Left arrow key
             switch (e.keyCode) {
                 case 37:
-                this.avatarImageSrc = "../public/img/player_left.png";
+                this.avatarImageSrc = "./public/img/player_left.png";
                    this.$store.state.steps += 1  
                    this.$store.state.stopWatch = true
                     avatarPosition.x -= 1
                     //Check if avatar has same position as any wall and has the drill active
                     if ((logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) && this.$store.state.drillActive) {
-                        walls[logic.findArrayElementIndex(wallPositions, avatarPosition)].style.display = "none";
+                        walls[logic.findArrayElementIndex(wallPositions, avatarPosition)].style.display = "none"
                         delete wallPositions[logic.findArrayElementIndex(wallPositions, avatarPosition)];
+                        delete collisionPositions[logic.findArrayElementIndex(collisionPositions, avatarPosition)];
                         avatarPosition.x += 1
                         this.$store.state.drillActive = false;
                         console.log('drill used');
@@ -141,15 +150,15 @@ export default {
                         this.$store.state.bombActive = false;
                         console.log('bomb used');
                     }
-                    //Check if avatar has same position as any wall.
-                    else if(logic.checkSamePosObjectList(avatarPosition, wallPositions) == false){
+                    //Check if avatar has same position as any wall or 'outside'.
+                    else if(logic.checkSamePosObjectList(avatarPosition, collisionPositions) == false){
                         avatarPosition.x += 1
                     }
                     //moves avatar to the left
                     else {
                         avatar.style.left = avatarPosition.x * unit + 'px'
                         //moves target to the left
-                        logic.moveTarget(avatar, avatarPosition, wallPositions, targetPositions, listZeroX, "left", target, targetPositions, this.$store.state.strengthActive)
+                        logic.moveTarget(avatar, avatarPosition, collisionPositions, targetPositions, listZeroX, "left", target, targetPositions, this.$store.state.strengthActive);
                     }
                     //checks if all targets are on the goal positions
                     if(logic.evaluateWin(goalPositions, targetPositions)){
@@ -165,13 +174,14 @@ export default {
                     break;
                 // Up arrow key   
                 case 38:
-                    this.avatarImageSrc = "../public/img/player_up.png";
+                    this.avatarImageSrc = "./public/img/player_up.png";
                     this.$store.state.steps += 1 
                     this.$store.state.stopWatch = true                   
                     avatarPosition.y -= 1
                     if ((logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) && this.$store.state.drillActive) {
                         walls[logic.findArrayElementIndex(wallPositions, avatarPosition)].style.display = "none";
                         delete wallPositions[logic.findArrayElementIndex(wallPositions, avatarPosition)];
+                        delete collisionPositions[logic.findArrayElementIndex(collisionPositions, avatarPosition)];
                         avatarPosition.y += 1
                         this.$store.state.drillActive = false;
                         console.log('no drill');
@@ -183,12 +193,12 @@ export default {
                         this.$store.state.bombActive = false;
                         console.log('bomb used');
                     }
-                    else if (logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) {
+                    else if (logic.checkSamePosObjectList(avatarPosition, collisionPositions) == false) {
                         avatarPosition.y += 1
                     }
                     else{
                         avatar.style.top = avatarPosition.y*unit + 'px'
-                        logic.moveTarget(avatar, avatarPosition, wallPositions, targetPositions, listZeroY, "up", target, targetPositions, this.$store.state.strengthActive)
+                        logic.moveTarget(avatar, avatarPosition, collisionPositions, targetPositions, listZeroY, "up", target, targetPositions, this.$store.state.strengthActive)
                     }
                     if(logic.evaluateWin(goalPositions, targetPositions)){
                         console.log("You have won.")
@@ -203,13 +213,14 @@ export default {
                     break;
                 // Right arrow key 
                 case 39:
-                    this.avatarImageSrc = "../public/img/player_right.png";
+                    this.avatarImageSrc = "./public/img/player_right.png";
                     this.$store.state.steps += 1
                     this.$store.state.stopWatch = true
                     avatarPosition.x += 1
                     if ((logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) && this.$store.state.drillActive) {
                         walls[logic.findArrayElementIndex(wallPositions, avatarPosition)].style.display = "none";
                         delete wallPositions[logic.findArrayElementIndex(wallPositions, avatarPosition)];
+                        delete collisionPositions[logic.findArrayElementIndex(collisionPositions, avatarPosition)];
                         avatarPosition.x -= 1
                         this.$store.state.drillActive = false;
                         console.log('no drill');
@@ -221,12 +232,12 @@ export default {
                         this.$store.state.bombActive = false;
                         console.log('bomb used');
                     }
-                    else if (logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) {
+                    else if (logic.checkSamePosObjectList(avatarPosition, collisionPositions) == false) {
                         avatarPosition.x -= 1
                     }
                     else{
                         avatar.style.left = avatarPosition.x *unit + 'px'
-                        logic.moveTarget(avatar, avatarPosition, wallPositions, targetPositions, listZeroX, "right", target, targetPositions, this.$store.state.strengthActive)
+                        logic.moveTarget(avatar, avatarPosition, collisionPositions, targetPositions, listZeroX, "right", target, targetPositions, this.$store.state.strengthActive)
                     }
                     if(logic.evaluateWin(goalPositions, targetPositions)){
                         console.log("You have won.")
@@ -242,13 +253,14 @@ export default {
                     break;
                 // Down arrow key
                 case 40:
-                    this.avatarImageSrc = "../public/img/player_down.png";
+                    this.avatarImageSrc = "./public/img/player_down.png";
                     this.$store.state.steps += 1
                     this.$store.state.stopWatch = true
                     avatarPosition.y += 1
                     if ((logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) && this.$store.state.drillActive) {
                         walls[logic.findArrayElementIndex(wallPositions, avatarPosition)].style.display = "none";
                         delete wallPositions[logic.findArrayElementIndex(wallPositions, avatarPosition)];
+                        delete collisionPositions[logic.findArrayElementIndex(collisionPositions, avatarPosition)];
                         avatarPosition.y -= 1
                         this.$store.state.drillActive = false;
                         console.log('no drill');
@@ -260,12 +272,12 @@ export default {
                         this.$store.state.bombActive = false;
                         console.log('bomb used');
                     }
-                    else if (logic.checkSamePosObjectList(avatarPosition, wallPositions) == false) {
+                    else if (logic.checkSamePosObjectList(avatarPosition, collisionPositions) == false) {
                         avatarPosition.y -= 1
                     }
                     else{
                         avatar.style.top = avatarPosition.y*unit + 'px'
-                        logic.moveTarget(avatar, avatarPosition, wallPositions, targetPositions, listZeroY, "down", target, targetPositions, this.$store.state.strengthActive)
+                        logic.moveTarget(avatar, avatarPosition, collisionPositions, targetPositions, listZeroY, "down", target, targetPositions, this.$store.state.strengthActive)
                     }
                     if(logic.evaluateWin(goalPositions, targetPositions)){
                        if (this.$store.state.level < this.$store.state.grids.length-1) {
